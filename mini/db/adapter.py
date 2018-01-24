50429f1ca73cf4e1c06f946b7ac0c6f1a2664d59
ambodi <amir.rahnama@gmail.com>
2018-01-24 09:19:42 +0100

    update the adapter and schema

--- mini/db/adapter.py
+++ mini/db/adapter.py
@@ -1,17 +1,17 @@
+"""DB adaper for the app."""
 from sqlite3 import dbapi2 as sqlite3
-from flask import Blueprint, request, session, g, redirect, url_for, abort, \
-     render_template, flash, current_app
+from flask import g, current_app
 
 
 def connect_db():
-    """Connects to the specific database."""
+    """Connect to the specific database."""
     rv = sqlite3.connect(current_app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 
 def init_db():
-    """Initializes the database."""
+    """Initialize the database."""
     db = get_db()
     with current_app.open_resource('db/schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
@@ -19,17 +19,72 @@ def init_db():
 
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
+    """Open a new database connection if there is none."""
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
 
 def get_user(username, password):
+    """Select user by username and password."""
     db = get_db()
-    cur = db.execute('select username, password from entries where username=?, \
-        password=?', (username, password))
-    entries = cur.fetchall()
+    cur = db.execute('select username, password from users where \
+      username=? AND password=?', (username, password))
+    entries = cur.fetchone()
     return entries
+
+
+def get_user_visit(username):
+    """Select users visit by username."""
+    db = get_db()
+    cur = db.execute('select counter from visits where username=?',
+                     (username,))
+    entries = cur.fetchone()
+    return entries
+
+
+def set_user_in_visits_table(username, visit):
+    """Set users visit."""
+    db = get_db()
+    db.execute('insert into visits (username, counter) values \
+         (?, ?)', (username, 1))
+    db.commit()
+
+    return 1
+
+
+def insert_user(username, password):
+    """Inser new users."""
+    db = get_db()
+    try:
+        db.execute('insert into users (username, password) values \
+         (?, ?)', (username, password))
+        db.commit()
+        created = True
+    except sqlite3.IntegrityError as e:
+        created = False
+
+    return created
+
+
+def update_user_visit(username, visit):
+    """Update the visit count of a user."""
+    new_visit = visit + 1
+    db = get_db()
+    db.execute('update visits set counter=? where \
+      username=?', (new_visit, username))
+    db.commit()
+
+    return new_visit
+
+
+def increment_visit(username):
+    """Select users visit by one."""
+    visit = get_user_visit(username)
+    no_visit = type(visit) is not sqlite3.Row
+
+    if (no_visit):
+        visit = 0
+        return set_user_in_visits_table(username, visit)
+
+    return update_user_visit(username, visit[0])
--- mini/db/schema.sql
+++ mini/db/schema.sql
@@ -7,7 +7,7 @@ create table users (
 drop table if exists visits;
 create table visits (
   username varchar(128) not null primary key,
-  visits integer not null
+  counter integer not null
 );
 
 insert into users (username, password) values ('admin@altavista.com','admin');
