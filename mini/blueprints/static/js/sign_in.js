ee74fdcd7ca3474c7090ade2ab941701b3a5ed48
ambodi <amir.rahnama@gmail.com>
2018-01-24 09:21:07 +0100

    improve the HTML and also refactor the script

--- mini/blueprints/static/js/sign_in.js
+++ mini/blueprints/static/js/sign_in.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-   document.getElementById('signup').addEventListener('click', function() {
-     print('he')
-     const username = document.getElementsByName("email").value;
-     const password = document.getElementsByName("password").value;
-
-     // const encrypted = CryptoJS.SHA256(...);
-     print(username, password)
-     if (username && password) {
-       fetch('http://localhost:5000/users/login', {
-         method: 'POST',
-         body: JSON.stringify({username, password}),
-         headers: new Headers({
-           'Content-Type': 'application/json'
-         })
-       })
-       .then(res => res.json())
-       .catch(error => console.error('Error:', error))
-       .then(response => console.log('Success:', response));
-     }
-   })
+   document.getElementById('signup').addEventListener('click', postData)
 }, false);
+
+
+function postData () {
+  const username = document.querySelector('input[name=email]').value;
+  const password = document.querySelector('input[name=password]').value;
+
+  if (username && password) {
+    return fetch('http://localhost:5000/users/login', {
+       method: 'POST',
+       body: JSON.stringify({username, password}),
+       headers: new Headers({
+         'Content-Type': 'application/json'
+       })
+     })
+     .then(res => res.json())
+     .catch(error => console.error('Error:', error))
+     .then(response => console.log('Success:', response));
+  }
+}
